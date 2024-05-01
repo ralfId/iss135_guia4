4bf2a1c0b44073e41f007ee9ccbde5fd032ba546
ID\ID <ralf_raid@outlook.es>
2024-05-01 17:03:36 -0600

    first commit

---
+++ src/main/java/com/iis/id24001/App.java
@@ -0,0 +1,18 @@
+package com.iis.id24001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello World";
+	}
+	
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println( hw.Hello() );
+    }
+}
